0a0a9bd33092bb8cb90c58c8e66b33f4b80d5e03
MD <mr.drobnych@gmail.com>
2018-11-26 13:02:14 +0200

    G

---
+++ app/models/profile.rb
@@ -0,0 +1,10 @@
+class Profile < ApplicationRecord
+	belongs_to :user
+
+    has_attached_file :avatar, styles: {thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+    validates :nickname, :age, :avatar, presence: true
+    validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 18, less_than: 65 }
+    validates :nickname, format: { with: /\A[a-zA-Z]+\z/}
+end
